28b311e62591d726b87a6358d1ab264d48dfa135
gyungbai98 <rglley98@naver.com>
2026-01-05 13:15:37 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 22384 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+
+    static int[] tmpArr;
+    static boolean[] selected;
+    static StringBuilder answer = new StringBuilder();
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int num : tmpArr) {
+                answer.append(num).append(" ");
+            }
+            answer.append("\n");
+
+            return;
+        }
+
+        for (int num = 1; num <= N; num++) {
+            if (selected[num]) {
+                continue;
+            }
+
+            selected[num] = true;
+            tmpArr[depth] = num;
+            dfs(depth + 1);
+            selected[num] = false;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        tmpArr = new int[M];
+        selected = new boolean[N + 1];
+        dfs(0);
+
+        System.out.println(answer);
+    }
+}
