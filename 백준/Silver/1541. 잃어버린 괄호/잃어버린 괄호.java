e51481228b5e2deedc1fac3a2caa54f8722deb29
이경배 <rglley98@naver.com>
2026-01-26 14:04:09 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -8,47 +8,18 @@ public class Main {
 
         String input = br.readLine();
         int sum = 0;
-        String posNum = "";
-        String negNum = "";
-        boolean isNeg = false;
-        for (int idx = 0; idx < input.length(); idx++) {
-            char ch = input.charAt(idx);
-            if ('0' <= ch && ch <= '9') {
-                if (isNeg) {
-                    negNum += ch;
-                } else {
-                    posNum += ch;
-                }
 
-                continue;
-            }
+        String[] arr = input.split("-");
+        for (String str : arr[0].split("\\+")) {
+            sum += Integer.parseInt(str);
+        }
 
-            if (ch == '-') {
-                if (isNeg) {
-                    sum -= Integer.parseInt(negNum);
-                    negNum = "";
-                } else {
-                    isNeg = true;
-                    sum += Integer.parseInt(posNum);
-                    posNum = "";
-                }
-            } else {
-                if(isNeg) {
-                    sum -= Integer.parseInt(negNum);
-                    negNum = "";
-                } else {
-                    sum += Integer.parseInt(posNum);
-                    posNum = "";
-                }
+        for (int idx = 1; idx < arr.length; idx++) {
+            for (String str : arr[idx].split("\\+")) {
+                sum -= Integer.parseInt(str);
             }
         }
 
-        if (!posNum.equals("")) {
-            sum += Integer.parseInt(posNum);
-        }
-        if (!negNum.equals("")) {
-            sum -= Integer.parseInt(negNum);
-        }
         System.out.println(sum);
     }
 }
