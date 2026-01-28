a5231b2f6d1014a8708bb445f764499aec78c668
이경배 <rglley98@naver.com>
2026-01-28 14:26:01 +0900

    [Gold III] Title: 파티, Time: 164 ms, Memory: 19872 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -3,72 +3,76 @@ import java.util.*;
 
 public class Main {
     static int N, M, X;
-    static List<int[]>[] adjacentList;
+    static List<int[]>[] adjList;
 
-    static final int INF = 1_000_000_000;
+    static int[] fromDistanceArr;
+    static int[] toDistanceArr;
+    static List<int[]>[] reversedAdjList;
 
-    static int[] dijkstra(int start) {
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, INF);
-        dist[start] = 0;
+    static void dijkstra(int[] distanceArr, List<int[]>[] adjList) {
+        Queue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        pq.offer(new int[]{start, 0});
+        Arrays.fill(distanceArr, Integer.MAX_VALUE);
+        distanceArr[X] = 0;
+        queue.offer(new int[]{X, 0});
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curNode = cur[0];
-            int curDist = cur[1];
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int currentNode = current[0];
+            int currentDistance = current[1];
 
-            // 이미 더 좋은 값으로 처리된 적 있으면 스킵
-            if (curDist != dist[curNode]) continue;
+            if (currentDistance > distanceArr[currentNode]) {
+                continue;
+            }
 
-            for (int[] edge : adjacentList[curNode]) {
-                int next = edge[0];
-                int w = edge[1];
+            for (int[] info : adjList[currentNode]) {
+                int nextNode = info[0];
+                int weight = info[1];
 
-                int nd = curDist + w;
-                if (nd < dist[next]) {
-                    dist[next] = nd;
-                    pq.offer(new int[]{next, nd});
+                int nextDistance = distanceArr[currentNode] + weight;
+                if(nextDistance < distanceArr[nextNode]) {
+                    distanceArr[nextNode] = nextDistance;
+                    queue.offer(new int[]{nextNode, nextDistance});
                 }
             }
         }
-
-        return dist;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
 
-        adjacentList = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) adjacentList[i] = new ArrayList<>();
+        adjList = new ArrayList[N + 1];
+        reversedAdjList = new ArrayList[N + 1];
+        for (int node = 1; node <= N; node++) {
+            adjList[node] = new ArrayList<>();
+            reversedAdjList[node] = new ArrayList<>();
+        }
 
-        for (int i = 0; i < M; i++) {
+        for (int edge = 1; edge <= M; edge++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            adjacentList[from].add(new int[]{to, w});
+            int weight = Integer.parseInt(st.nextToken());
+
+            adjList[from].add(new int[]{to, weight});
+            reversedAdjList[to].add(new int[]{from, weight});
         }
 
-        // 1) X -> 모든 노드
-        int[] distFromX = dijkstra(X);
+        fromDistanceArr = new int[N + 1];
+        dijkstra(fromDistanceArr, adjList);
+        toDistanceArr = new int[N + 1];
+        dijkstra(toDistanceArr, reversedAdjList);
 
-        // 2) i -> X (역방향 없이: i마다 다익스트라)
-        int ans = 0;
-        for (int i = 1; i <= N; i++) {
-            int[] distFromI = dijkstra(i);
-            int go = distFromI[X];     // i -> X
-            int back = distFromX[i];   // X -> i
-            ans = Math.max(ans, go + back);
+        int max = 0;
+        for (int node = 1; node <= N; node++) {
+            max = Math.max(max, fromDistanceArr[node] + toDistanceArr[node]);
         }
 
-        System.out.println(ans);
+        System.out.println(max);
     }
 }
