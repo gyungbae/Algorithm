6bcec33409ce2f6c7babd5dde8952338cae0d1d0
이경배 <rglley98@naver.com>
2026-02-02 15:36:47 +0900

    [Gold II] Title: 사전, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Gold/1256. 사전/사전.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M; 
+    static long K;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        K = Long.parseLong(input[2]);
+
+        long[][] dp = new long[N + 1][M + 1];
+        for (int aCount = 0; aCount <= N; aCount++) {
+            for (int zCount = 0; zCount <= M; zCount++) {
+                if (aCount == 0 || zCount == 0) {
+                    dp[aCount][zCount] = 1;
+                    continue;
+                }
+
+                dp[aCount][zCount] = Math.min(dp[aCount - 1][zCount] + dp[aCount][zCount - 1], K);
+            }
+        }
+
+        if (dp[N][M] < K) {
+            System.out.println(-1);
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (N > 0 && M > 0) {
+            long length = dp[N - 1][M];
+
+            if (K <= length) {
+                sb.append("a");
+                N--;
+            } else {
+                sb.append("z");
+                K -= length;
+                M--;
+            }
+        }
+
+        while (N > 0) {
+            sb.append("a");
+            N--;
+        }
+
+        while (M > 0) {
+            sb.append("z");
+            M--;
+        }
+
+        System.out.println(sb);
+    }
+}
