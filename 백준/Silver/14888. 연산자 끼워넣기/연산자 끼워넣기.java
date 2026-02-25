cdb90d565465e8efe0b145b5451ef90735ec5354
이경배 <rglley98@naver.com>
2026-02-25 16:35:44 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 144 ms, Memory: 19616 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -9,43 +9,36 @@ public class Main {
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
 
-    static void DFS(int sum, int count0, int count1, int count2, int count3, int depth) {
+    static void DFS(int sum, int depth) {
         if (depth == N) {
             max = Math.max(max, sum);
             min = Math.min(min, sum);
             return;
         }
-
-        for (int operator = 0; operator < 4; operator++) {
-            if (isUsable(operator, count0, count1, count2, count3)) {
-                switch (operator) {
-                    case 0 -> DFS(sum + inputArr[depth], count0 + 1, count1, count2, count3, depth + 1);
-                    case 1 -> DFS(sum - inputArr[depth], count0, count1 + 1, count2, count3, depth + 1);
-                    case 2 -> DFS(sum * inputArr[depth], count0, count1, count2 + 1, count3, depth + 1);
-                    case 3 -> DFS(sum / inputArr[depth], count0, count1, count2, count3 + 1, depth + 1);
-                }
-            }
+        
+        int next = inputArr[depth];
+        if (countArr[0] > 0) {
+            countArr[0]--;
+            DFS(sum + next, depth + 1);
+            countArr[0]++;
         }
-    }
 
-    static boolean isUsable(int operator, int count0, int count1, int count2, int count3) {
-        switch (operator) {
-            case 0 -> {
-                if(countArr[operator] == count0) return false;
-                else return true;
-            }
-            case 1 -> {
-                if(countArr[operator] == count1) return false;
-                else return true;
-            }
-            case 2 -> {
-                if(countArr[operator] == count2) return false;
-                else return true;
-            }
-            default -> {
-                if(countArr[operator] == count3) return false;
-                else return true;
-            }
+        if (countArr[1] > 0) {
+            countArr[1]--;
+            DFS(sum - next, depth + 1);
+            countArr[1]++;
+        }
+
+        if (countArr[2] > 0) {
+            countArr[2]--;
+            DFS(sum * next, depth + 1);
+            countArr[2]++;
+        }
+
+        if (countArr[3] > 0) {
+            countArr[3]--;
+            DFS(sum / next, depth + 1);
+            countArr[3]++;
         }
     }
 
@@ -65,7 +58,7 @@ public class Main {
             countArr[idx] = Integer.parseInt(st.nextToken());
         }
 
-        DFS(inputArr[0], 0, 0, 0, 0, 1);
+        DFS(inputArr[0], 1);
 
         System.out.println(max + "\n" + min);
     }
