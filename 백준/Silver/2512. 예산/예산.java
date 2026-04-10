388757e197b79426ba4843a99fea83839c8b0e0c
이경배 <rglley98@naver.com>
2026-04-10 13:18:30 +0900

    [Silver II] Title: 예산, Time: 140 ms, Memory: 15392 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -2,20 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
         int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long require = 0;
         int max = 0;
-        for (int idx = 0; idx < N; idx++) {
-            int value = Integer.parseInt(st.nextToken());
-            arr[idx] = value;
-            require += value;
-            max = Math.max(max, value);
+        int require = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+            require += arr[i];
         }
 
         int M = Integer.parseInt(br.readLine());
@@ -23,27 +23,26 @@ public class Main {
             System.out.println(max);
             return;
         }
-
-        int left = 0;
+        
+        int left = 1;
         int right = max;
-        max = 0;
+        int answer = 0;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            long sum = 0;
-            for (int budget : arr) {
-                sum += Math.min(budget, mid);
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                sum += Math.min(arr[i], mid);
             }
 
             if (sum <= M) {
-                max = mid;      
-                left = mid + 1;    
+                answer = mid;
+                left = mid + 1;
             } else {
-                right = mid - 1;   
+                right = mid - 1;
             }
         }
 
-        System.out.println(max);
+        System.out.println(answer);
     }
 }
