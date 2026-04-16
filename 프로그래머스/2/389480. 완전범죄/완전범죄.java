9cf4f671a1b55bb11486e97f5a0f162e4007acab
이경배 <rglley98@naver.com>
2026-04-16 10:19:28 +0900

    [level 2] Title: 완전범죄, Time: 0.31 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -2,39 +2,42 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] info, int n, int m) {
-        int answer = n;
+        int answer = Integer.MAX_VALUE;
         
         int size = info.length;
         int[][] dp = new int[size][m];
-        for(int idx = 0; idx < size; idx++) {
-            Arrays.fill(dp[idx], n);
+        for(int row = 0; row < size; row++) {
+            Arrays.fill(dp[row], n);
         }
         
-        if(info[0][0] < n) 
+        if(info[0][0] < n)
             dp[0][0] = info[0][0];
         
-        if(info[0][1] < m) 
+        if(info[0][1] < m)
             dp[0][info[0][1]] = 0;
         
-        for(int idx = 1; idx < size; idx++) {
-            int valueA = info[idx][0];
-            int valueB = info[idx][1];
+        for(int row = 1; row < size; row++) {
+            int printA = info[row][0];
+            int printB = info[row][1];
             
-            for(int printB = 0; printB < m; printB++) {
-                dp[idx][printB] = Math.min(dp[idx][printB], dp[idx - 1][printB] + valueA);
-                
-                int nextPrintB = printB + valueB;
-                if(nextPrintB >= m)
+            for(int col = 0; col < m; col++) {
+                if(dp[row - 1][col] == n)
                     continue;
                 
-                dp[idx][nextPrintB] = Math.min(dp[idx][nextPrintB], dp[idx - 1][printB]);
+                int nextA = dp[row - 1][col] + printA;
+                if(nextA < n)
+                    dp[row][col] = Math.min(dp[row][col], nextA);
+                
+                int nextB = col + printB;
+                if(nextB < m)
+                    dp[row][nextB] = Math.min(dp[row][nextB], dp[row - 1][col]); 
             }
         }
         
-        for(int printB = 0; printB < m; printB++) {
-            answer = Math.min(answer, dp[size - 1][printB]);
+        for(int col = 0; col < m; col++) {
+            answer = Math.min(dp[size - 1][col], answer);
         }
-         
+        
         return answer == n ? -1 : answer;
     }
 }
