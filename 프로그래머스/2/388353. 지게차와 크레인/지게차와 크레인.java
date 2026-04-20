d47914ea9705091160af912e3ba0e9074d692a1a
이경배 <rglley98@naver.com>
2026-04-20 18:57:18 +0900

    [level 2] Title: 지게차와 크레인, Time: 33.11 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,47 +1,21 @@
 import java.util.*;
 
-/**
-request의 길이가 1일 경우 상하좌우 탐색으로 외곽과의 연결여부 확인
-※요청이 들어온 순간 접근 가능한 -> 일괄 삭제 필요
-request의 길이가 2일 경우 모두 삭제
-DFS 풀이
-**/
-
 class Solution {
-    int rowSize;
-    int colSize;
-    char[][] chStorage;
-    boolean[][] removed;
+    int rowSize, colSize;
+    char[][] map;
     
+    boolean[][] outside;
+    boolean[][] removed;
     int[] deltaRow = {-1, 1, 0, 0};
     int[] deltaCol = {0, 0, -1, 1};
     
-    void request1(String request) {
-        char command = request.charAt(0);
-        List<int[]> toRemove = new ArrayList<>();
-        
-        for(int row = 0; row < rowSize; row++) {
-            for(int col = 0; col < colSize; col++) {
-                if(removed[row][col] || chStorage[row][col] != command) 
-                    continue;
-                
-                if(canRemove(row, col)) {
-                    toRemove.add(new int[]{row, col});
-                }
-            }
-        }
-        
-        for(int[] info : toRemove) {
-            removed[info[0]][info[1]] = true;
-        }
-    }
-    
-    boolean canRemove(int row, int col) {
+    void checkOutside() {
         Queue<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[rowSize][colSize];
+        boolean[][] visited = new boolean[rowSize + 2][colSize + 2];
+        outside = new boolean[rowSize + 2][colSize + 2];
         
-        visited[row][col] = true;
-        queue.offer(new int[]{row, col});
+        visited[0][0] = true;
+        queue.offer(new int[]{0, 0});
         
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
@@ -50,28 +24,59 @@ class Solution {
                 int nextRow = current[0] + deltaRow[delta];
                 int nextCol = current[1] + deltaCol[delta];
                 
-                if(nextRow < 0 || nextRow >= rowSize || nextCol < 0 || nextCol >= colSize) 
-                    return true;
-                
-                if(visited[nextRow][nextCol] || !removed[nextRow][nextCol])
+                if(nextRow < 0 || nextRow > rowSize + 1 || nextCol < 0 || nextCol > colSize + 1)
                     continue;
                 
-                visited[nextRow][nextCol] = true;
-                queue.offer(new int[]{nextRow, nextCol});
+                if(visited[nextRow][nextCol])
+                    continue;
+                
+                if(1 <= nextRow && nextRow <= rowSize && 1 <= nextCol && nextCol <= colSize){
+                    if(!removed[nextRow][nextCol]) {
+                        outside[nextRow][nextCol] = true;
+                    } else {
+                        visited[nextRow][nextCol] = true;
+                        queue.offer(new int[]{nextRow, nextCol});
+                    }
+                } else {
+                    visited[nextRow][nextCol] = true;
+                    queue.offer(new int[]{nextRow, nextCol});
+                }
+            }
+        } 
+    }
+    
+    int getRemain() {
+        int count = 0;
+        for(int row = 1; row <= rowSize; row++) {
+            for(int col = 1; col <= colSize; col++) {
+                if(!removed[row][col])
+                    count++;
             }
         }
         
-        return false;
+        return count;
     }
     
-    void request2(String request) {
-        char command = request.charAt(0);
-        
-        for(int row = 0; row < rowSize; row++) {
-            for(int col = 0; col < colSize; col++) {
-                if(chStorage[row][col] == command) {
+    void remove1(char target) {
+        for(int row = 1; row <= rowSize; row++) {
+            for(int col = 1; col <= colSize; col++) {
+                if(removed[row][col])
+                    continue;
+                
+                if(map[row][col] == target && outside[row][col])
                     removed[row][col] = true;
-                }       
+            }
+        }
+    }
+    
+    void remove2(char target) {
+        for(int row = 1; row <= rowSize; row++) {
+            for(int col = 1; col <= colSize; col++) {
+                if(removed[row][col])
+                    continue;
+                
+                if(map[row][col] == target)
+                    removed[row][col] = true;
             }
         }
     }
@@ -81,31 +86,27 @@ class Solution {
         
         rowSize = storage.length;
         colSize = storage[0].length();
-        chStorage = new char[rowSize][colSize];
-        for(int row = 0; row < rowSize; row++) {
-            String line = storage[row];
-            for(int col = 0; col < colSize; col++) {
-                chStorage[row][col] = line.charAt(col);
+        
+        map = new char[rowSize + 1][colSize + 1];
+        for(int row = 1; row <= rowSize; row++) {
+            char[] chArr = storage[row - 1].toCharArray();
+            for(int col = 1; col <= colSize; col++) {
+                map[row][col] = chArr[col - 1];
             }
         }
         
-        removed = new boolean[rowSize][colSize];
+        outside = new boolean[rowSize + 1][colSize + 1];
+        removed = new boolean[rowSize + 1][colSize + 1];
         for(String request : requests) {
-            if(request.length() == 1) {
-                request1(request);
-                continue;
-            }
+            checkOutside();
             
-            request2(request);
-        }
-        
-        for(int row = 0; row < rowSize; row++) {
-            for(int col = 0; col < colSize; col++) {
-                if(!removed[row][col])
-                    answer++;
+            if(request.length() == 1) {
+                remove1(request.charAt(0));
+            } else {
+              remove2(request.charAt(0));  
             }
         }
         
-        return answer;
+        return getRemain();
     }
 }
