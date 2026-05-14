afba0eea7215477ceb13b0d24529ed04c25c6e03
이경배 <rglley98@naver.com>
2026-05-14 12:36:44 +0900

    [level 2] Title: 리프 노드 수 최대화, Time: 75.25 ms, Memory: 58.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
@@ -0,0 +1,34 @@
+class Solution {
+    int distLimit, splitLimit;
+    int answer;
+
+    void search(long currentDist, long leaf, long split, long used) {
+        if (used > distLimit) return;
+
+        answer = (int) Math.max(answer, currentDist + leaf);
+
+        for (int child = 2; child <= 3; child++) {
+            long nextSplit = split * child;
+            if (nextSplit > splitLimit) continue;
+
+            long nextNodes = currentDist * child;
+            long remain = distLimit - used;
+
+            long nextDist = Math.min(remain, nextNodes);
+            long nextLeaf = nextNodes - nextDist;
+
+            search(nextDist, leaf + nextLeaf, nextSplit, used + nextDist);
+        }
+    }
+
+    public int solution(int dist_limit, int split_limit) {
+        this.distLimit = dist_limit;
+        this.splitLimit = split_limit;
+
+        answer = 1;
+
+        search(1, 0, 1, 1);
+
+        return answer;
+    }
+}
