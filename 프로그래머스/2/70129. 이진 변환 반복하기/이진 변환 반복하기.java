f9370c200f2049a066b8cc43c740c902dd4d0195
이경배 <rglley98@naver.com>
2026-05-27 13:09:04 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 383.54 ms, Memory: 359 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,23 +1,45 @@
 class Solution {
-    public int[] solution(String s) {
-        int count = 0;      
-        int zeroCount = 0;  
-
-        while (!s.equals("1")) {
-            int oneCount = 0;
-
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '1') {
-                    oneCount++;
-                } else {
-                    zeroCount++;
-                }
-            }
-
-            s = Integer.toBinaryString(oneCount);
-            count++;
+    int[] answer;
+    
+    void convert(String current, int depth, int zero) {
+        if(current.equals("1")) {
+            answer[0] = depth;
+            answer[1] = zero;
+            return;
         }
-
-        return new int[]{count, zeroCount};
+        
+        String tmpNext = "";
+        for(int idx = 0; idx < current.length(); idx++) {
+            if(current.charAt(idx) == '1')
+                tmpNext += "1";
+        }
+        
+        int tmpLength = tmpNext.length();
+        int nextLength = tmpLength;
+        
+        int divisor = 1;
+        while(divisor * 2 <= tmpLength) {
+            divisor *= 2;
+        }
+        
+        String next = "";
+        while(divisor > 0) {
+            if(nextLength >= divisor) {
+                nextLength -= divisor;
+                next += "1";
+            } else {
+                next += "0";
+            }
+            
+            divisor /= 2;
+        }
+        
+        convert(next, depth + 1, zero + current.length() - tmpNext.length());
+    }
+    
+    public int[] solution(String s) {
+        answer = new int[2];
+        convert(s, 0, 0);
+        return answer;
     }
 }
