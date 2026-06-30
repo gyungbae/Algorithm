0e847c1c7060fc0f37a21d76e8c55ebc971136d4
이경배 <rglley98@naver.com>
2026-06-30 14:34:51 +0900

    [level 2] Title: N개의 최소공배수, Time: 1.72 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,24 +1,26 @@
-class Solution {
-    
-    int gcd(int firstNumber, int secondNumber) {
-        while(secondNumber != 0) {
-            int remainder = firstNumber % secondNumber;
-            firstNumber = secondNumber;
-            secondNumber = remainder;
-        }
+import java.util.*;
 
-        return firstNumber;
+class Solution {
+    int getGCD(int num1, int num2) {
+        while(num2 != 0) {
+            int remain = num1 % num2;
+            num1 = num2;
+            num2 = remain;
+        }
+        
+        return num1;
     }
     
-    int lcm(int firstNumber, int secondNumber) {
-        return firstNumber / gcd(firstNumber, secondNumber) * secondNumber;
+    int getLCM(int num1, int num2) {
+        return num1 * num2 / getGCD(num1, num2);
     }
     
     public int solution(int[] arr) {
-        int answer = arr[0];
+        Arrays.sort(arr);
         
-        for(int arrIdx = 1; arrIdx < arr.length; arrIdx++) {
-            answer = lcm(answer, arr[arrIdx]);
+        int answer = 1;
+        for(int idx = arr.length - 1; idx >= 0; idx--) {
+            answer = getLCM(answer, arr[idx]);
         }
         
         return answer;
