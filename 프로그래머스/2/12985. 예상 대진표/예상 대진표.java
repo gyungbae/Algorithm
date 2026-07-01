3233d908261b6529800269637956adfecae1de1f
이경배 <rglley98@naver.com>
2026-07-01 13:03:20 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,20 +1,14 @@
 class Solution {
     public int solution(int n, int a, int b) {
         int answer = 0;
+        
         while(true) {
+            answer++;
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            
             if(a == b)
                 break;
-            
-            answer++;
-            
-            if(a % 2 != 0)
-                a++;
-            
-            if(b % 2 != 0)
-                b++;
-            
-            a /= 2;
-            b /= 2;
         }
         
         return answer;
