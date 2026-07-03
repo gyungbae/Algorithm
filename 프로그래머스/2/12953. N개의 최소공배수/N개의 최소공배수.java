b7659f6285c080f13a7cc3225ec2808b50d607b7
이경배 <rglley98@naver.com>
2026-07-03 14:14:19 +0900

    [level 2] Title: N개의 최소공배수, Time: 1.30 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     int getGCD(int num1, int num2) {
-        while(num2 != 0) {
+        while(num2 > 0) {
             int remain = num1 % num2;
             num1 = num2;
             num2 = remain;
