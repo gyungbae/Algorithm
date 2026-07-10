b5acf897db5ab21dcbf71dc208398c9eba4c8bdf
이경배 <rglley98@naver.com>
2026-07-10 13:04:51 +0900

    [level 1] Title: K번째수, Time: 1.57 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        int answerIdx = 0;
+        for(int[] command : commands) {
+            int i = command[0];
+            int j = command[1];
+            int k = command[2];
+            
+            int[] arr = Arrays.copyOfRange(array, i - 1, j);
+            Arrays.sort(arr);
+            answer[answerIdx++] = arr[k - 1];
+        }
+        
+        return answer;
+    }
+}
