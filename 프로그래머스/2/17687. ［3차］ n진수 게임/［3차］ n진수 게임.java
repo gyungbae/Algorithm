126e096f0c3050416d013fabd233dd7b745d106a
이경배 <rglley98@naver.com>
2026-07-14 16:04:09 +0900

    [level 2] Title: [3차] n진수 게임, Time: 7.46 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,34 +1,20 @@
 class Solution {
-    String convert(int num, int base) {
-        if(num == 0)
-            return "0";
-        
-        String digitString = "0123456789ABCDEF";
-        
-        StringBuilder result = new StringBuilder();
-        while(num > 0) {
-            int digit = num % base;
-            
-            result.append(digitString.charAt(digit));
-            
-            num /= base;
-        }
-        
-        return result.reverse().toString();
-    }
-    
     public String solution(int n, int t, int m, int p) {
-        StringBuilder sb = new StringBuilder();
+        int maxLength = m * t;
         
+        StringBuilder sb = new StringBuilder();
         int num = 0;
-        while(sb.length() <= t * m) {
-            sb.append(convert(num, n));
-            num++;
+        while(sb.length() < maxLength) {
+            sb.append(Integer.toString(num++, n).toUpperCase());
         }
         
+        String str = sb.toString();
         StringBuilder answer = new StringBuilder();
-        for(int idx = p - 1; t-- > 0; idx += m) {
-            answer.append(sb.charAt(idx));
+        int strIdx = p - 1;
+        while(t > 0) {
+            answer.append(str.charAt(strIdx));
+            strIdx += m;
+            t--;
         }
         
         return answer.toString();
