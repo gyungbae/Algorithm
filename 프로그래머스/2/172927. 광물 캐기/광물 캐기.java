9d2ee773e43d295a1ff2ba1a3d711f142186e4f5
이경배 <rglley98@naver.com>
2026-08-02 13:00:03 +0900

    [level 2] Title: 광물 캐기, Time: 1.25 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -4,53 +4,44 @@ class Solution {
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
         
-        int pickCount = 0;
-        
-        for(int pick : picks) {
-            pickCount += pick;
-        }
-        
-        int mineralCount = Math.min(minerals.length, pickCount * 5);
+        int pickCount = picks[0] + picks[1] + picks[2];
+        int maxMineralIdx = Math.min(pickCount * 5, minerals.length) - 1;
         
         List<int[]> infoList = new ArrayList<>();
-        
-        for(int headIdx = 0; headIdx < mineralCount; headIdx += 5) {
-            int diamondCount = 0;
-            int ironCount = 0;
-            int stoneCount = 0;
+        for(int headIdx = 0; headIdx <= maxMineralIdx; headIdx += 5) {
+            int diamond = 0;
+            int iron = 0;
+            int stone = 0;
             
-            for(int idx = headIdx; idx < headIdx + 5 && idx < mineralCount; idx++) {
+            for(int idx = headIdx; idx < headIdx + 5 && idx <= maxMineralIdx; idx++) {
                 String mineral = minerals[idx];
                 
-                if(mineral.equals("diamond")) {
-                    diamondCount++;
-                } else if(mineral.equals("iron")) {
-                    ironCount++;
-                } else {
-                    stoneCount++;
-                }
+                if(mineral.equals("diamond"))
+                    diamond++;
+                else if(mineral.equals("iron"))
+                    iron++;
+                else
+                    stone++;
             }
             
-            int score = 25 * diamondCount + 5 * ironCount + stoneCount;
-            infoList.add(new int[]{diamondCount, ironCount, stoneCount, score});
+            int fatigue = diamond * 25 + iron * 5 + stone;
+            infoList.add(new int[]{diamond, iron, stone, fatigue});
         }
         
         Collections.sort(infoList, (o1, o2) -> o2[3] - o1[3]);
         
         for(int[] info : infoList) {
             if(picks[0] > 0) {
-                answer += info[0];
-                answer += info[1];
-                answer += info[2];
+                answer += (info[0] + info[1] + info[2]);
                 picks[0]--;
             } else if(picks[1] > 0) {
-                answer += info[0] * 5;
-                answer += info[1];
-                answer += info[2];
+                answer += (info[0] * 5 + info[1] + info[2]);
                 picks[1]--;
-            } else if(picks[2] > 0){
+            } else if(picks[2] > 0) {
                 answer += info[3];
                 picks[2]--;
+            } else {
+                break;
             }
         }
         
