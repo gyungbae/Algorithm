6045a0d1807f89521e508767d991dbca585b6e41
이경배 <rglley98@naver.com>
2026-08-07 17:48:38 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 3.41 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,85 +1,84 @@
 import java.util.*;
 
 class Solution {
-    char[][] map;
     int m, n;
+    char[][] board;
     
-    List<int[]> deleteList;
+    boolean isSame(int row, int col, char ch) {
+        if(ch == '0')
+            return false;
+        
+        if(row + 1 >= m || col + 1 >= n)
+            return false;
+        
+        if(board[row + 1][col] != ch || board[row][col + 1] != ch ||
+          board[row + 1][col + 1] != ch)
+            return false;
+        
+        return true;
+    }
     
-    void delete() {
-        for(int[] info : deleteList) {
+    void crack(List<int[]> crackList) {
+        for(int[] info : crackList) {
             int row = info[0];
             int col = info[1];
-            
-            map[row][col] = '.';
-            map[row + 1][col] = '.';
-            map[row][col + 1] = '.';
-            map[row + 1][col + 1] = '.';
+            board[row][col] = '0';
+            board[row + 1][col] = '0';
+            board[row][col + 1] = '0';
+            board[row + 1][col + 1] = '0';
         }
         
-        for (int col = 0; col < n; col++) {
-            int emptyRow = m - 1;
-
-            for (int row = m - 1; row >= 0; row--) {
-                if (map[row][col] == '.')
-                    continue;
-
-                map[emptyRow][col] = map[row][col];
-
-                if (emptyRow != row)
-                    map[row][col] = '.';
-
-                emptyRow--;
-            }
-
-            while (emptyRow >= 0) {
-                map[emptyRow][col] = '.';
-                emptyRow--;
-            }
+        for(int col = 0; col < n; col++) {
+            down(col);
+        }
+    }
+    
+    void down(int col) {
+        int zeroRow = m - 1;
+        for(int row = m - 1; row >= 0; row--) {
+            if(board[row][col] == '0')
+                continue;
+            
+            char current = board[row][col];
+            board[row][col] = '0';
+            board[zeroRow][col] = current;
+            zeroRow--;
         }
     }
     
     public int solution(int m, int n, String[] board) {
         this.m = m;
         this.n = n;
+        this.board = new char[m][n];
         
-        map = new char[m][n];
         for(int row = 0; row < m; row++) {
-            for(int col = 0; col < n; col++) {
-                map[row][col] = board[row].charAt(col);
-            }
+            this.board[row] = board[row].toCharArray();
         }
         
         while(true) {
-            deleteList = new ArrayList<>();
+            List<int[]> crackList = new ArrayList<>();
             
             for(int row = 0; row < m - 1; row++) {
                 for(int col = 0; col < n - 1; col++) {
-                    char ch = map[row][col];
-                    
-                    if(ch == '.')
-                        continue;
-                    
-                    if(map[row + 1][col] == ch && map[row][col + 1] == ch && map[row + 1][col + 1] == ch)
-                        deleteList.add(new int[]{row, col});
+                    if(isSame(row, col, this.board[row][col]))
+                        crackList.add(new int[]{row, col});
                 }
             }
             
-            if(deleteList.size() == 0)
+            if(crackList.size() == 0)
                 break;
             
-            delete();
+            crack(crackList);
         }
         
-        int answer = 0;
-        
-        for(int row = 0; row < m; row++) {
-            for(int col = 0; col < n; col++) {
-                if(map[row][col] == '.')
-                    answer++;
+        int remainCount = 0;
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (this.board[row][col] != '0')
+                    remainCount++;
             }
         }
-        
-        return answer;
+
+        return m * n - remainCount;
     }
 }
